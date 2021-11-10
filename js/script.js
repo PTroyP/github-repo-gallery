4a603e2da451643fe33e20f55fcba5c1220805f8
troy_p <troyparker55@gmail.com>
2021-11-10 10:18:59 -0600

    Added userDisplay

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// div for profile information
+const overview = document.querySelector(".overview");
+const username = "PTroyP";
+
+const profileInfo = async function() {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    console.log(data);
+    userDisplay(data);
+}
+profileInfo();
+
+const userDisplay = function(data) {
+    const userInfo = document.createElement("div");
+    userInfo.classList.add("user-info");
+    userInfo.innerHTML = `<figure>
+        <img alt="user avatar" src =${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>`;
+    overview.append(userInfo);
+    
+}
