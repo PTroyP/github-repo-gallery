a748e8a3834b7e099cbe06d846a82454a3dfad2f
troy_p <troyparker55@gmail.com>
2021-11-10 15:12:33 -0600

    Added function to list repos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // div for profile information
 const overview = document.querySelector(".overview");
 const username = "PTroyP";
+const reposList = document.querySelector(".repo-list");
 
+// retrieve user info from github
 const profileInfo = async function() {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
@@ -10,9 +12,11 @@ const profileInfo = async function() {
 }
 profileInfo();
 
+// function to display user information 
 const userDisplay = function(data) {
     const userInfo = document.createElement("div");
     userInfo.classList.add("user-info");
+    // add html elements to userInfo div
     userInfo.innerHTML = `<figure>
         <img alt="user avatar" src =${data.avatar_url} />
         </figure>
@@ -22,6 +26,24 @@ const userDisplay = function(data) {
             <p><strong>Location:</strong> ${data.location}</p>
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`;
+        // append new elements to userInfo div
     overview.append(userInfo);
-    
+    grabRepos();
 }
+
+const grabRepos = async function() {
+    const resRepo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const dataRepo = await resRepo.json();
+    console.log(dataRepo);
+    individualRepo(dataRepo);
+}
+// grabRepos();
+
+const individualRepo = function(repos) {
+    // creating list items in the ul that display the repos
+    for(let entry of repos){
+        const listItem = document.createElement("li");
+        listItem.classList.add("repo");
+        listItem.innerHTML = `<h3>${entry.name}</h3>`;
+        reposList.append(listItem);
+}}
